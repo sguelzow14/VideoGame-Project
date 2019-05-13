1613a69703931811629438fdfc53924a8cd7a5cc
Justin Horner <Horner1@stolaf.edu>
2019-05-13 18:39:38 -0500

    Commentary

--- MetaGames/app.R
+++ MetaGames/app.R
@@ -21,6 +21,15 @@ ui <- fluidPage(
          plotOutput("distPlot")
       
    )),
+   mainPanel(
+     h1("First level title"),
+     h2("Second level title"),
+     h3("Third level title"),
+     h4("Fourth level title"),
+     h5("Fifth level title"),
+     h6("Sixth level title")
+   ),
+   
   mainPanel(
     plotOutput("NextPlot")
   ),
